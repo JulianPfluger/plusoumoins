68381daf6cce3268d31f0f8f7c5b2df285bd2543
PtitJulian <pfluger2u@etu.univ-lorraine.fr>
2022-11-08 20:05:17 +0100

    lvl2 added

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ let number = document.querySelector("#number");
 let play = document.querySelector("#play");
 let reponse = document.querySelector("#reponse");
 let replay = document.querySelector("#replay");
+const NO_OF_HIGH_SCORES = 10;
+const HIGH_SCORES = 'highScores';
 
 
 function getRandomInt(max) {
@@ -16,7 +18,7 @@ function compare() {
 
     compt++;
 
-    if (compt < 5 || number.value == trueNumber) {
+    if (compt < 10 || number.value == trueNumber) {
         if (number.value <= 100 && number.value >= 0 && number.value != "") {
 
             if (number.value < trueNumber) {
@@ -30,6 +32,7 @@ function compare() {
             else if (number.value == trueNumber) {
                 reponse.innerHTML = "Bravo, le nombre est bien " + trueNumber + "<br>Vous avez réussi en " + compt + " tentative(s)";
                 replay.style.opacity = 1;
+                gameOver(compt);
             }
         }
 
@@ -49,6 +52,47 @@ function compare() {
     number.value = "";
 
 }
+
+function checkHighScore(score) {
+    const highScores = JSON.parse(localStorage.getItem(HIGH_SCORES)) ?? [];
+    const lowestScore = highScores[NO_OF_HIGH_SCORES - 1]?.score ?? 0;
+    
+    if (score > lowestScore) {
+      saveHighScore(score, highScores);
+      showHighScores();
+    }
+  }
+
+  function gameOver(score) {
+    // Other game over logic.
+    checkHighScore(score);
+  }
+
+  function saveHighScore(score, highScores) {
+    const name = prompt('You got a highscore! Enter name:');
+    const newScore = { score, name };
+    
+    // 1. Add to list
+    highScores.push(newScore);
+  
+    // 2. Sort the list
+    highScores.sort((a, b) => a.score - b.score);
+    
+    // 3. Select new list
+    highScores.splice(NO_OF_HIGH_SCORES);
+    
+    // 4. Save to local storage
+    localStorage.setItem(HIGH_SCORES, JSON.stringify(highScores));
+  };
+
+  function showHighScores() {
+    const highScores = JSON.parse(localStorage.getItem(HIGH_SCORES)) ?? [];
+    const highScoreList = document.getElementById(HIGH_SCORES);
+    
+    highScoreList.innerHTML = highScores
+      .map((score) => `<li>${score.score} - ${score.name}`)
+      .join('');
+  }
 
 function reload() {
     replay.style.opacity = 0;
